dd57ca3fd83d9844f9e80b38d3a41d9d229cadc6
dwhddnjs <per1215@naver.com>
2021-10-12 22:56:50 +0900

    practiced for dictionary

--- dictionary.py
+++ dictionary.py
@@ -4,8 +4,20 @@
     'model':'520d'
     }
 
+car = {
+    'brand': 'KIA',
+    'model': 'K5',
+    'color': 'black'
+}
+
 print(중고차['brand']) #bmw
 
 중고차['brand'] = 123
 
 print(중고차['brand']) #123
+
+print(car['model']) 
+
+car['model'] = 'k9'
+
+print(['model']) 
